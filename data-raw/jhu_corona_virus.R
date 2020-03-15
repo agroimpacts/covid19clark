241cec2d47c0c3b9d553d52a61b3628561fcf667
ldemaz <3401881+ldemaz@users.noreply.github.com>
2020-03-15 15:49:33 -0400

    First commit of covid19clark

---
+++ data-raw/jhu_corona_virus.R
@@ -0,0 +1,27 @@
+# Download JHU Corona virus data
+# source: https://github.com/CSSEGISandData/COVID-19
+# code adapted from: https://github.com/RamiKrispin/coronavirus
+
+library(tidyverse)
+jhu_path <- glue::glue("https://raw.githubusercontent.com/CSSEGISandData/",
+                       "COVID-19/master/csse_covid_19_data/",
+                       "csse_covid_19_time_series/",
+                       "time_series_19-covid-Confirmed.csv")
+cases_raw <- jhu_dat <- readr::read_csv(file = jhu_path)
+
+# wide to long
+cases_long <- cases_raw %>%
+  pivot_longer(cols = 5:ncol(.), names_to = "date", values_to = "cases") %>%
+  mutate(date = lubridate::as_date(lubridate::mdy(date))) %>%
+  rename(admin = `Province/State`, ctry = `Country/Region`,
+         y = Lat, x = Long) %>% dplyr::select(admin, ctry, x, y, date, cases)
+
+# write to inst/extdata
+readr::write_csv(cases_long,
+                 path = here::here("inst/extdata/covid19_daily.csv"))
+
+# build package
+devtools::install(build_vignettes = TRUE)
+rmarkdown::render(input = here::here('README.Rmd'),
+                  output_file = here::here("README.md"))
+
