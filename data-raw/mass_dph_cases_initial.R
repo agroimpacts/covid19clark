9e4f1ae110238989c9009f3251e55ce577d8da7c
ldemaz <3401881+ldemaz@users.noreply.github.com>
2020-03-20 17:51:09 -0400

    Package update on 2020-03-20 17:51:09

--- data-raw/mass_dph_cases_initial.R
+++ data-raw/mass_dph_cases_initial.R
@@ -1,11 +1,10 @@
 # get old cases from DPH
-library(covid19clark)
 `%>%` <- magrittr::`%>%`
 
 dvec <- paste0("march-", sprintf("%02i", 13:17), "-2020")
 dph_tabs <- lapply(dvec, function(x) {  # x <- dvec[3]
   print(x)
-  get_dph_cases(x)
+  covid19clark::get_dph_cases(dph_path = x)
 }) %>% do.call(rbind, .)
 
 readr::write_csv(dph_tabs,
