89efee74cff734a35449e2db9c4cee25eace8424
ldemaz <3401881+ldemaz@users.noreply.github.com>
2020-04-04 08:31:18 -0400

    Update April 4. X-axis labels rotated

--- data-raw/jhu_corona_virus_daily_initial.R
+++ data-raw/jhu_corona_virus_daily_initial.R
@@ -2,7 +2,7 @@ library(lubridate)
 library(dplyr)
 
 dvec <- c(paste0("03-", sprintf("%02i", 1:25), "-2020"),
-          paste0("04-", sprintf("%02i", 1), "-2020"))
+          paste0("04-", sprintf("%02i", 3), "-2020"))
 
 daily_reports <- lapply(dvec, function(x) {
   get_jhu_daily(download_date = x, write = FALSE)
