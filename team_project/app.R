90ffcb18a929c0549b6ee36c6d465165712d142a
Maymon <ZMaymon@clarku.edu>
2020-04-21 22:52:25 -0400

    Edits to the Shiny COVID19 application

--- team_project/app.R
+++ team_project/app.R
@@ -1,7 +1,9 @@
 library(shiny)
 library(leaflet)
 library(RColorBrewer)
+# install.packages("COVID19")
 library(COVID19)
+# install.packages("covid19clark")
 library(covid19clark)
 library(scales)
 library(lattice)
@@ -11,10 +13,11 @@ library(rsconnect)
 data("us_cases_daily")
 
 ui <- bootstrapPage(
-  titlePanel("COVID19 Data"),
+  titlePanel("COVID19 Daily Cases/Deaths Data"),
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 70, right = 10,
+
                 selectInput("extent", "Geographic Extent", list("County", "State"))
                 ,
                 selectInput("colors", "Color Scheme",
@@ -49,34 +52,50 @@ server <- function(input, output, session) {
   observe({
     colorBy <- input$color
     sizeBy <- input$size
+    us_cases_county <- us_cases_daily$county %>%
+      filter((cases > 0) & (!is.na(cases)))
+    us_cases_county_max <- us_cases_county %>% filter(date == max(date))
+
+    us_cases_state <- us_cases_daily$state %>%
+      filter((cases > 0) & (!is.na(cases)))
+    us_cases_state_max <- us_cases_state %>% filter(date == max(date))
+
+    us_deaths_county <- us_cases_daily$county %>%
+      filter((deaths > 0) & (!is.na(deaths)))
+    us_deaths_county_max <- us_deaths_county %>% filter(date == max(date))
+
+    us_deaths_state <- us_cases_daily$state %>%
+      filter((deaths > 0) & (!is.na(deaths)))
+    us_deaths_state_max <- us_deaths_county %>% filter(date == max(date))
 
     if (input$extent == "County") {
-      extentBy <- us_cases_daily$county
+      extentBy <-  us_cases_county
       if (input$caseordeath == "Cases") {
-        values_COVID <- extentBy$cases
+        values_COVID <- us_cases_county$cases
         scalar <- 500
         opacity <- .5
       } else {
-        values_COVID <- extentBy$deaths
-        scalar <- 2000
-        opacity <- .25
+        values_COVID <- us_deaths_county$deaths
+        scalar <- 1000
+        opacity <- .5
       }
     } else {
-      extentBy <- us_cases_daily$state
+      extentBy <- us_cases_state
       if (input$caseordeath == "Cases") {
-        values_COVID <- extentBy$cases
+        values_COVID <- us_cases_state$cases
         scalar <- 500
         opacity <- .5
       } else {
-        values_COVID <- extentBy$deaths
-        scalar <- 500
-        opacity <- .25
+        values_COVID <- us_deaths_state$deaths
+        scalar <- 1000
+        opacity <- .5
       }
     }
 
 
+
     colorData <- input$colors
-    pal <- colorFactor(input$colors, values_COVID)
+    pal <- colorBin(pal = input$colors, domain = values_COVID, bins = 8)
 
 
     #binpal <- colorBin("Blues", us_cases_daily$state)
@@ -95,4 +114,5 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui, server)
+
 
