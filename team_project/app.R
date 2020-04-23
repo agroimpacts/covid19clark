b193c95395fc0aeaaa9baf2d33ae8905894245e5
Maymon <ZMaymon@clarku.edu>
2020-04-23 00:17:02 -0400

    Changes to settings to dataconstruction

--- team_project/app.R
+++ team_project/app.R
@@ -21,7 +21,7 @@ ui <- bootstrapPage(
   titlePanel("COVID19 Daily Cases/Deaths Data"),
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
-  absolutePanel(top = 70, right = 10,
+  absolutePanel(top = 100, right = 10,
 
                 selectInput("extent", "Geographic Extent", list("County", "State"))
                 ,
@@ -30,6 +30,10 @@ ui <- bootstrapPage(
                 ),
                 selectInput("caseordeath", "Cases/Deaths", list("Cases", "Deaths"))
                 ,
+                dateInput(inputId = "dateinput", label = "Input Date",
+                          value = max(us_cases_county_max$date),
+                          min = min(us_cases_county$date),
+                          max = max(us_cases_county$date)),
                 checkboxInput("legend", "Show legend", TRUE)
 
 
@@ -43,33 +47,6 @@ server <- function(input, output, session) {
 
   ## Interactive Map ###########################################
 
-  warn_messages <- reactiveVal(NULL)
-
-  # turn any warnings produced by calling a function
-  # into a notification and optionally call the function
-  # again to return the results (use `return = FALSE` if
-  # function produces side effects)
-  catchWarning <- function(f, ..., return = TRUE) {
-    tryCatch(f(...), warning = function(w) {
-      isolate({
-        msgs <- c(warn_messages(), w$message)
-        warn_messages(msgs)
-      })
-      if (return) f(...)
-    })
-  }
-  output$inputs <- renderUI({
-    tagList(
-
-      catchWarning(dateRangeInput, "x4", "Mis-specified `start`", start = "null"),
-    )
-  })
-  outputOptions(output, "inputs", suspendWhenHidden = FALSE)
-
-  observe ({
-    catchWarning(updateDateRangeInput, session, "x4", end = "x", return = FALSE)
-  })
-
   # Create the map
   output$map <- renderLeaflet({
     leaflet() %>%
@@ -77,49 +54,57 @@ server <- function(input, output, session) {
         urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
         attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
       ) %>%
-      setView(lng = -93.85, lat = 37.45, zoom = 4)
+      setView(lng = -93.85, lat = 37.45, zoom = 4.5)
   })
 
 
   observe({
     colorBy <- input$color
     sizeBy <- input$size
+
     us_cases_county <- us_cases_daily$county %>%
       filter((cases > 0) & (!is.na(cases)))
-    us_cases_county_max <- us_cases_county %>% filter(date == max(date))
+    us_cases_county_max <- us_cases_county %>% filter(date == input$dateinput)
 
     us_cases_state <- us_cases_daily$state %>%
       filter((cases > 0) & (!is.na(cases)))
-    us_cases_state_max <- us_cases_state %>% filter(date == max(date))
+    us_cases_state_max <- us_cases_state %>% filter(date == input$dateinput)
 
     us_deaths_county <- us_cases_daily$county %>%
       filter((deaths > 0) & (!is.na(deaths)))
-    us_deaths_county_max <- us_deaths_county %>% filter(date == max(date))
+    us_deaths_county_max <- us_deaths_county %>% filter(date == input$dateinput)
 
     us_deaths_state <- us_cases_daily$state %>%
       filter((deaths > 0) & (!is.na(deaths)))
-    us_deaths_state_max <- us_deaths_county %>% filter(date == max(date))
+    us_deaths_state_max <- us_deaths_state %>% filter(date == input$dateinput)
 
     if (input$extent == "County") {
-      extentBy <-  us_cases_county
+
       if (input$caseordeath == "Cases") {
-        values_COVID <- us_cases_county$cases
+        us_value_vector <- us_cases_county_max
+        extentBy <-  us_cases_county_max
+        values_COVID <- us_cases_county_max$cases
         scalar <- 500
         opacity <- .5
       } else {
-        values_COVID <- us_deaths_county$deaths
-        scalar <- 1000
+        us_value_vector <- us_deaths_county_max
+        extentBy <-  us_deaths_county_max
+        values_COVID <- us_deaths_county_max$deaths
+        scalar <- 1500
         opacity <- .5
       }
-    } else {
-      extentBy <- us_cases_state
-      if (input$caseordeath == "Cases") {
-        values_COVID <- us_cases_state$cases
+    }else if (input$extent == "State") {
+      if (input$caseordeath == "cases") {
+        us_value_vector <- us_cases_state_max
+        extentBy <-  us_cases_state_max
+        values_COVID <- us_cases_state_max$cases
         scalar <- 500
         opacity <- .5
       } else {
-        values_COVID <- us_deaths_state$deaths
-        scalar <- 1000
+        us_value_vector <- us_deaths_state_max
+        extentBy <-  us_deaths_state_max
+        values_COVID <- us_deaths_state_max$deaths
+        scalar <- 1500
         opacity <- .5
       }
     }
@@ -131,12 +116,12 @@ server <- function(input, output, session) {
 
 
     #binpal <- colorBin("Blues", us_cases_daily$state)
-    leafletProxy("map", data = us_cases_daily) %>%
+    leafletProxy("map", data = us_value_vector) %>%
       clearShapes() %>%
       addCircles(~extentBy$x, ~extentBy$y,
                  stroke=FALSE, fillOpacity=opacity,
                  weight = 1, radius = ~sqrt(values_COVID)*scalar,
-                 popup = ~as.character(paste0(extentBy$county.x, sep = ", ", extentBy$state2)),
+                 popup = ~as.character(paste0(extentBy$county.x, sep = ", ", extentBy$state1)),
                  label = ~as.character(paste0("Amount of", sep = " ", input$caseordeath, sep = ": ", values_COVID)),
                  color = ~pal(values_COVID)) %>%
       addLegend("topleft", pal=pal, values= ~values_COVID, title=colorBy,
