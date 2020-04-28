2696095a0844a908d8316354caebb3466bc06711
Maymon <ZMaymon@clarku.edu>
2020-04-28 15:42:56 -0400

    log10 additions to code

--- team_project/app.R
+++ team_project/app.R
@@ -3,7 +3,7 @@ library(leaflet)
 library(RColorBrewer)
 # install.packages("COVID19")
 library(COVID19)
-# install.packages("covid19clark")
+#install.packages("covid19clark")
 library(covid19clark)
 library(scales)
 library(lattice)
@@ -91,34 +91,34 @@ server <- function(input, output, session) {
         us_value_vector <- us_cases_county_max
         extentBy <-  us_cases_county_max
         values_COVID <- us_cases_county_max$cases
-        scalar <- 500
-        opacity <- .5
+        scalar <- 5000
+        opacity <- 1
       } else {
         us_value_vector <- us_deaths_county_max
         extentBy <-  us_deaths_county_max
         values_COVID <- us_deaths_county_max$deaths
-        scalar <- 500
-        opacity <- .5
+        scalar <- 5000
+        opacity <- 1
       }
     }else if (input$extent == "State") {
       if (input$caseordeath == "cases") {
         us_value_vector <- us_cases_state_max
         extentBy <-  us_cases_state_max
         values_COVID <- us_cases_state_max$cases
-        scalar <- 500
-        opacity <- .5
+        scalar <- 5000
+        opacity <- 1
       } else {
         us_value_vector <- us_deaths_state_max
         extentBy <-  us_deaths_state_max
         values_COVID <- us_deaths_state_max$deaths
-        scalar <- 500
-        opacity <- .5
+        scalar <- 5000
+        opacity <- 1
       }
     }
 
 
     colorData <- input$colors
-    pal <- colorQuantile(pal = input$colors, domain = values_COVID, n = 8)
+    pal <- colorBin(pal = input$colors, domain = values_COVID, bins = 8)
 
 
     #binpal <- colorBin("Blues", us_cases_daily$state)
@@ -126,7 +126,7 @@ server <- function(input, output, session) {
       clearShapes() %>%
       addCircles(~extentBy$x, ~extentBy$y,
                  stroke=FALSE, fillOpacity=opacity,
-                 weight = 1, radius = ~sqrt(values_COVID)*scalar,
+                 weight = 1, radius = log10(values_COVID) * scalar,
                  popup = ~as.character(paste0(extentBy$county.x, sep = ", ", extentBy$state1)),
                  label = ~as.character(paste0("Amount of", sep = " ", input$caseordeath, sep = ": ", values_COVID)),
                  color = ~pal(values_COVID)) %>%
